5aa3f3f93813b248656efeb9b2b8e945f4a65ee0
aaronvwalker <aaronvwalker@gmail.com>
2016-07-18 22:09:11 -0400

    Creating

---
+++ WebApi2Book.Data/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Data.Entities
+{
+    public class Task
+    {
+        private readonly IList<User> _users = new List<User>();
+
+        public virtual long TaskId { get; set; }
+        public virtual string Subject { get; set; }
+        public virtual DateTime? StartDate { get; set; }
+        public virtual DateTime? DueDate { get; set; }
+    }
+}
